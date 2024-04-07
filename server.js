a23239b3ab1af8d6d117844f166cc1b67781308a
Pritiranjan Pradhan <128985988+prpradhan13@users.noreply.github.com>
2024-04-07 08:57:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,18 +6,12 @@ import authRoute from './routes/authRoute.js'
 import categoryRoute from './routes/categoryRoute.js'
 import fileRoute from './routes/fileRoute.js'
 import dietRoute from './routes/dietRoute.js'
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 // config env
 dotenv.config();
 
 // Database configuration
 connectDB();
-
-//esmodule fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -30,11 +24,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/category', categoryRoute);
 app.use('/api/file', fileRoute);
 app.use('/api/diet', dietRoute);
-app.use(express.static(path.join(__dirname, './client')))
 
-app.use('*', function(req, res){
-    res.sendFile(path.join(__dirname, './client/index.html'))
-})
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, (req, res) => {
